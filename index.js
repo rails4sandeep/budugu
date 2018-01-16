c8e9e0eb74dc335d00e82992ba4ee2f9d5b997be
Sandeep Kumar <sandeep.kumar@vocus.com.au>
2018-01-16 15:32:19 +1100

    Budugu to reply to people messages disabled

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const TeleBot = require('telebot');
 const bot = new TeleBot('547814548:AAHTmQHNMzDxNPGHb0q4Z2wsLI-eiavtbHY');
 // bot.on('text', (msg) => msg.reply.text(msg.text));
-bot.on('text', (msg) => msg.reply.text('nice ' + msg.from.first_name));
+// bot.on('text', (msg) => msg.reply.text('nice ' + msg.from.first_name));
 bot.start();
 
 bot.on('newChatMembers', (msg) => msg.reply.text('Welcome '  + msg.new_chat_members[0].first_name + ' to MANAM - Mana Australia Telugu Association. Please share a small introduction about you to the group. You can find more information about MANAM at www.manam-au.org'));
