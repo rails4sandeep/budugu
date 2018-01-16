262bc2759dffb0612922217e24e80053abdeee4b
Sandeep Kumar <sandeep.kumar@vocus.com.au>
2018-01-16 14:52:47 +1100

    Budugu is ready to rock

---
+++ index.js
@@ -0,0 +1,15 @@
+const TeleBot = require('telebot');
+const bot = new TeleBot('547814548:AAHTmQHNMzDxNPGHb0q4Z2wsLI-eiavtbHY');
+// bot.on('text', (msg) => msg.reply.text(msg.text));
+// bot.on('text', (msg) => msg.reply.text(msg.from.first_name));
+bot.start();
+
+bot.on('newChatMembers', (msg) => msg.reply.text('Welcome '  + msg.new_chat_members[0].first_name + ' to MANAM - Mana Australia Telugu Association. Please share a small introduction about you to the group. You can find more information about MANAM at www.manam-au.org'));
+
+// bot.on('newChatMembers', (msg) => {
+//   return bot.sendMessage(msg.from.id, `Hello, ${ msg.from.first_name }!`);
+// });
+
+// bot.on('text', (msg) => {
+//   return bot.sendMessage('text', (msg) => msg.reply.text(msg.from.first_name));
+// });
