c44d9220d1c3020a2c0ff16cdb55fda365fc46ea
Sandeep Kumar <sandeep.kumar@vocus.com.au>
2018-01-19 16:01:25 +1100

    Add appreciation for sharing job opportunities

--- suryakantham.js
+++ suryakantham.js
@@ -1,11 +1,18 @@
 const TeleBot = require('telebot');
 const bot = new TeleBot('535241451:AAFdc2dpzWl9Q0rlEuRamlCwxOuvbbmlI20');
 const bannedWords = ['fuck','asshole','ysr','jagan','chandrababu','chandrababu naidu','chandra babu naidu','lokesh','tdp','kcr','ktr','trs','free voucher','freevoucher','rajasekhar reddy','చంద్రబాబు','cheppandi'];
+const favoriteWords = ['opportunity','openings','job','jobs','opening','role','roles'];
 
 bot.on('text', (msg) => {
-    bannedWords.forEach((word) => {
-        if(msg.text.includes(word)) {
+    bannedWords.forEach((badWord) => {
+        if(msg.text.includes(badWord)) {
             msg.reply.text('Your message might be in violation of the MANAM rules & regulations. MANAM wishes to keep its forums free of politics & abuse. Thank you');
+        }
+    });
+    
+    favoriteWords.forEach((goodWord) => {
+        if(msg.text.includes(goodWord)) {
+            msg.reply.text('Thanks for sharing this opportunity with MANAM community!');
         }
     });
 });
