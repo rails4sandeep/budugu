b0d467e78b3e7dc31ea7b44dab8da84e775c4b3e
Sandeep Kumar <sandeep.kumar@vocus.com.au>
2018-01-19 14:58:54 +1100

    Add triggering by command cheppandi

--- suryakantham.js
+++ suryakantham.js
@@ -1,6 +1,6 @@
 const TeleBot = require('telebot');
 const bot = new TeleBot('535241451:AAFdc2dpzWl9Q0rlEuRamlCwxOuvbbmlI20');
-const bannedWords = ['fuck','asshole','ysr','jagan','chandrababu','chandrababu naidu','chandra babu naidu','lokesh','tdp','kcr','ktr','trs','free voucher','freevoucher','rajasekhar reddy'];
+const bannedWords = ['fuck','asshole','ysr','jagan','chandrababu','chandrababu naidu','chandra babu naidu','lokesh','tdp','kcr','ktr','trs','free voucher','freevoucher','rajasekhar reddy','చంద్రబాబు','cheppandi'];
 
 bot.on('text', (msg) => {
     bannedWords.forEach((word) => {
@@ -12,6 +12,9 @@ bot.on('text', (msg) => {
 
 bot.start();
 
+bot.on('/cheppandi', (msg) => {
+    msg.reply.text('Your message might be in violation of the MANAM rules & regulations. MANAM wishes to keep its forums free of politics & abuse. Thank you');
+  });
 
 
 
