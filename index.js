6d3a7a8ed10506f3d4dca8597b76b1af53a589cd
Sandeep Kumar <sandeep.kumar@vocus.com.au>
2018-02-07 12:03:07 +1100

    Refactor and clean budugu code. Make it more readable

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 const TeleBot = require('telebot');
 const bot = new TeleBot('547814548:AAHTmQHNMzDxNPGHb0q4Z2wsLI-eiavtbHY');
 
-//bot.on('text', (msg) => msg.reply.text(msg.text));
-
 bot.on('text', (msg) => {
-    if(msg.text.toLowerCase().includes('budugu')) {
-        msg.reply.text('thank you andi');
-    }
+    budugu(msg);
 });
 
-bot.on('photo', (msg) => msg.reply.text('good one ' + msg.from.first_name + ' garu'));
+bot.on('photo', (msg) => photo(msg));
 
 bot.start();
 
+bot.on('newChatMembers', (msg) => newChatMembers(msg));
 
-bot.on('newChatMembers', (msg) => msg.reply.text('Welcome '  + msg.new_chat_members[0].first_name + ' to MANAM - Mana Australia Telugu Association. Please share a small introduction about you to the group. You can find more information about MANAM at www.manam-au.org'));
+var budugu = (msg) => {
+    if(msg.text.toLowerCase().includes('budugu')) {
+        msg.reply.text('thank you andi');
+    }
+};
 
-// bot.on('newChatMembers', (msg) => {
-//   return bot.sendMessage(msg.from.id, `Hello, ${ msg.from.first_name }!`);
-// });
+var photo = (msg) => {
+    msg.reply.text('good one ' + msg.from.first_name + ' garu');
+};
 
-// bot.on('text', (msg) => {
-//   return bot.sendMessage('text', (msg) => msg.reply.text(msg.from.first_name));
-// });
+var newChatMembers = (msg) => {
+    msg.reply.text('Welcome '  + msg.new_chat_members[0].first_name + ' to MANAM - Mana Australia Telugu Association. Please share a small introduction about you to the group. You can find more information about MANAM at www.manam-au.org');
+};
