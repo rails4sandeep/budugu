3a53397fbff9ab229712a9c7c95794d0a2f14781
Sandeep Kumar <sandeep.kumar@vocus.com.au>
2018-02-07 11:42:27 +1100

    Refactor suryakantham code. Make it clean and readable

--- suryakantham.js
+++ suryakantham.js
@@ -1,41 +1,66 @@
+//declarations
 const TeleBot = require('telebot');
 const bot = new TeleBot('535241451:AAFdc2dpzWl9Q0rlEuRamlCwxOuvbbmlI20');
 const bannedWords = ['fuck','asshole','ysr','jagan','chandrababu','chandrababu naidu','chandra babu naidu','lokesh','tdp','kcr','ktr','trs','free voucher','freevoucher','rajasekhar reddy','చంద్రబాబు'];
-const favoriteWords = ['opportunity','openings','job','jobs','opening','role','roles'];
+const favoriteWords = ['opportunity','openings','job','jobs','opening','role','roles','thank'];
 let goodWordFlag = true;
 let badWordFlag = true;
+
+//bot responses
 bot.on('text', (msg) => {
+
+    cheppandi(msg);
+    bannedWordsWarning(msg,bannedWords);
+    goodWordsAppreciation(msg, favoriteWords);
+    sankrantiSpecial(msg);
+
+    goodWordFlag = true;
+    badWordFlag = true;
+});
+
+//start the bot
+bot.start();
+
+//evarumeeru command response
+bot.on('/evarumeeru', (msg) => {
+    aboutMe(msg);
+  });
+
+
+//methods
+var cheppandi = (msg) => {
     if(msg.text.toLowerCase().includes('cheppandi')) {
         msg.reply.text('cheppadaniki emi ledu pani chusukondi');
     }
+};  
 
+var bannedWordsWarning = (msg, bannedWords) => {
     bannedWords.forEach((badWord) => {
         if(msg.text.toLowerCase().includes(badWord) && badWordFlag) {
             msg.reply.text('Your message might be in violation of the MANAM rules & regulations. MANAM wishes to keep its forums free of politics & clean. Thank you');
             badWordFlag = false;
         }
     });
-    
+};
+
+var goodWordsAppreciation = (msg, favoriteWords) => {
     favoriteWords.forEach((goodWord) => {
         if(msg.text.toLowerCase().includes(goodWord) && goodWordFlag) {
             msg.reply.text("👍");
             goodWordFlag = false;            
         }
     });
+};
 
-    if(msg.text.toLowerCase().includes('Manam Sankranti Sambaraalu')) {
+var sankrantiSpecial = (msg) => {
+    if(msg.text.toLowerCase().includes('manam sankranti sambaraalu')) {
         msg.reply.text('Manam Sankranti Sambaraalu 28 Jan 2018 Redum Centre Wentworthville lo jarigayi. Photolu ivigo' +
          'https://photos.app.goo.gl/jR4VPkeGgVXoPiON2');
     }
+};
 
-    goodWordFlag = true;
-    badWordFlag = true;
-});
-
-bot.start();
-
-  bot.on('/evarumeeru', (msg) => {
+var aboutMe = (msg) => {
     msg.reply.text('I am the official moderator of MANAM telegram group. I will take care of the group. I appreciate good posts & inform when posts are in violation of MANAM policies');
-  });
+};
 
 
